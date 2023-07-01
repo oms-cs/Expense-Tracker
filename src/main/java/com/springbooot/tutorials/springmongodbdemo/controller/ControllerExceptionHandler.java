46bbfdef3d329d42848f0b77d34118ea978b5e42
newbieprogrammer17 <omshinde1706@gmail.com>
2023-07-01 21:29:52 +0530

    Expense Tracker With MongoDB with Full Text Search

---
+++ src/main/java/com/springbooot/tutorials/springmongodbdemo/controller/ControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.springbooot.tutorials.springmongodbdemo.controller;
+
+
+import com.springbooot.tutorials.springmongodbdemo.exception.NotFoundException;
+import com.springbooot.tutorials.springmongodbdemo.model.CustomErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<CustomErrorResponse> notFoundException(NotFoundException e) {
+        CustomErrorResponse error = new CustomErrorResponse(HttpStatus.NOT_FOUND.toString(), e.getMessage());
+        error.setStatus((HttpStatus.NOT_FOUND.value()));
+        error.setTimestamp(LocalDateTime.now());
+        ResponseEntity<CustomErrorResponse> responseEntity = new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        return responseEntity;
+    }
+}
